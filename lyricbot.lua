b1e1c3fe22d21d47c135e2503021d572a700af9c
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2022-03-04 18:44:56 +0100

    Create lyricbot.lua

---
+++ lyricbot.lua
@@ -0,0 +1,71 @@
+-- Settings
+local Type = "booth"; -- "booth", "sign"
+local Richtext = [[
+<stroke thickness="2">Karaoke Bot</stroke>
+<font color="#D4AF37">%s</font>
+]];
+local Lyrics = [[
+Somebody once told me the world is gonna roll me
+I ain't the sharpest tool in the shed
+She was looking kind of dumb with her finger and her thumb
+In the shape of an "L" on her forehead
+Well the years start coming and they don't stop coming
+Fed to the rules and I hit the ground running
+Didn't make sense not to live for fun
+Your brain gets smart but your head gets dumb
+So much to do, so much to see
+So what's wrong with taking the back streets?
+You'll never know if you don't go
+You'll never shine if you don't glow
+Hey now, you're an all-star, get your game on, go play
+Hey now, you're a rock star, get the show on, get paid
+And all that glitters is gold
+Only shooting stars break the mold
+It's a cool place and they say it gets colder
+You're bundled up now, wait 'til you get older
+But the meteor men beg to differ
+Judging by the hole in the satellite picture
+The ice we skate is getting pretty thin
+The water's getting warm so you might as well swim
+My world's on fire, how about yours?
+That's the way I like it and I'll never get bored
+Hey now, you're an all-star, get your game on, go play
+Hey now, you're a rock star, get the show on, get paid
+All that glitters is gold
+Only shooting stars break the mold
+Hey now, you're an all-star, get your game on, go play
+Hey now, you're a rock star, get the show, on get paid
+And all that glitters is gold
+Only shooting stars
+Somebody once asked could I spare some change for gas?
+I need to get myself away from this place
+I said, "Yup" what a concept
+I could use a little fuel myself
+And we could all use a little change
+Well, the years start coming and they don't stop coming
+Fed to the rules and I hit the ground running
+Didn't make sense not to live for fun
+Your brain gets smart but your head gets dumb
+So much to do, so much to see
+So what's wrong with taking the back streets?
+You'll never know if you don't go (go!)
+You'll never shine if you don't glow
+Hey now, you're an all-star, get your game on, go play
+Hey now, you're a rock star, get the show on, get paid
+And all that glitters is gold
+Only shooting stars break the mold
+And all that glitters is gold
+Only shooting stars break the mold
+]];
+
+-- Main
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local EditBooth = ReplicatedStorage.Events.EditBooth;
+while true do
+    for _, Line in next, Lyrics:split("\n") do
+        if (type(Line) == "string" and Line ~= "") then
+            EditBooth:FireServer(Richtext:format(Line), Type);
+            task.wait(#Line:split(" ") / 140 * 60);
+        end
+    end
+end
